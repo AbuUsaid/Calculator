141d53453ccd2e5d057c5a79084a83bb9e76c672
AbuUsaid <usaidabu321@gmail.com>
2023-05-13 18:39:05 +0530

    add index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log('Calculator console');
+
+//-----functions---------
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+const sub = (num1, num2) => {
+  return num1 - num2;
+};
+
+const mul = (num1, num2) => {
+  return num1 * num2;
+};
+
+const div = (num1, num2) => {
+  return num1 / num2;
+};
+//--------functions--------
+
+/*
+let number1 = 1;
+let number2 = 2;
+let operator = 'sum';
+
+const operate = (operator, num1, num2) => {
+  if (operator === 'sum') {
+    return add(num1, num2);
+  }
+};
+
+console.log(operate(operator, number1, number2));
+*/
