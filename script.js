65857c091a1db763d1eb2cb7e21dfc8ec7e87e69
AbuUsaid <usaidabu321@gmail.com>
2023-05-20 20:41:17 +0530

    script.js and style.css added

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 console.log('Calculator console');
 
-let value = '';
+let value1 = '';
+let value2 = '';
+let value1andOperator = '';
+let operator = '';
 
 //Get the display element
 const display = document.querySelector('.display');
 
 //Get all the number buttons
-const buttons = document.querySelectorAll('.button');
+const numbers = document.querySelectorAll('.number');
+const operations = document.querySelectorAll('.operation');
 
 //Add event listeners to each button
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    value = value + e.target.innerText;
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    value1 = value1 + e.target.innerText; //Number1
 
-    display.innerText = value;
+    display.innerText = value1;
+
+    operations.forEach((op) => {
+      op.addEventListener('click', (e) => {
+        operator = e.target.innerText; //Operator use switch to calculate
+        value1andOperator = value1 + operator; //This is just for display
+        console.log(operator + ' ' + typeof operator);
+
+        display.innerText = value1andOperator;
+      });
+    });
   });
 });
